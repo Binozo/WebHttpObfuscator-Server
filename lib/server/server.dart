946d6721d97b30bd35a150c581b28f81095f1c82
Binozo <entwickler@binozoworks.de>
2022-11-27 21:38:19 +0100

    added basic server code

---
+++ lib/server/server.dart
@@ -0,0 +1,57 @@
+import 'dart:io';
+
+import 'package:shelf/shelf_io.dart' as shelf_io;
+import 'package:shelf_web_socket/shelf_web_socket.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
+import 'package:webhttpobfuscator_server/log/log.dart';
+
+class Server {
+  final Function(String) _payloadEncryptor;
+  final Function(String) _payloadDecryptor;
+  final int _port;
+  HttpServer? _websocketServer;
+
+  Server(this._port, this._payloadEncryptor, this._payloadDecryptor);
+
+  void serve(Function(String) handleRequestCallback) async {
+    var handler = webSocketHandler((WebSocketChannel webSocket) {
+      final stream = webSocket.stream.listen((message) {
+        // Read encrypted json payload
+        final String encryptedPayload = message;
+        Log.debug("Received payload");
+
+        // Trying to decrypt it...
+        String decrypted = "";
+
+        try {
+          Log.debug("Trying to decrypt payload...");
+          decrypted = _payloadDecryptor(encryptedPayload);
+        } catch (e) {
+          Log.warning("Couldn't decrypt payload. May be malformed. Disconnecting...");
+          webSocket.sink.close();
+          return;
+        }
+
+        Log.debug("Processing request...");
+        // Process the request and send the payload back to the client
+        final String result = handleRequestCallback(decrypted);
+
+        // Encrypt it again
+        final String encrypted = _payloadEncryptor(result);
+
+        // Send it back and close connection
+        webSocket.sink.add(encrypted);
+        webSocket.sink.close();
+        Log.debug("Successfully processed request. Stream closed.");
+      });
+    });
+
+    _websocketServer = await shelf_io.serve(handler, "localhost", _port);
+    Log.info("Serving Websocket on Port $_port");
+    Log.info("Connect your client to ws://${_websocketServer!.address.host}:$_port");
+  }
+
+  void shutdown() async {
+    await _websocketServer?.close();
+  }
+}
