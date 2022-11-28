23ce2bf6ce78f35e494d019a76633645bdde135a
Binozo <entwickler@binozoworks.de>
2022-11-28 17:47:53 +0100

    prepared example server code

--- bin/webhttpobfuscator_server.dart
+++ bin/webhttpobfuscator_server.dart
@@ -1,10 +1,19 @@
-
-
 import 'package:webhttpobfuscator_server/client/client.dart';
 import 'package:webhttpobfuscator_server/server/server.dart';
 
+const port = 8080;
+String payloadEncryptor(String payload) {
+  // TODO
+  return payload;
+}
+
+String payloadDecryptor(String payload) {
+  // TODO
+  return payload;
+}
+
 void main(List<String> arguments) {
-  final server = Server(8080, (payload) => payload, (payload) => payload);
+  final server = Server(port, (payload) => payloadEncryptor, (payload) => payloadDecryptor);
   server.serve((request) async {
     return await Client.performRequest(request);
   });
